036e46ffcde3b7c167562c2fbd78f8473776eb2b
lmartinez <lmartinezj@gmail.com>
2021-03-18 21:24:51 -0500

    tried another hack to disable dotenv in prod

--- app.js
+++ app.js
@@ -29,6 +29,10 @@ app.use(cookieParser())
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+if (process.env.NODE_ENV !== 'production') { 
+    require('dotenv').config() 
+ }
+
 app.use(cors())
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
