cdbee7ca095d45074259fb1b1a6f1d4e72105e4e
lmartinez <lmartinezj@gmail.com>
2021-03-21 02:09:38 -0500

    Added Refunds to flow, missing response to request

--- app.js
+++ app.js
@@ -153,4 +153,48 @@ app.post('/payments/:paymentid/authorizations', async (req, res, next) => {
     .catch(error => console.log("Error app.js: " + error.message))
 });
 
+app.post('/payments/:paymentid/captures', async (req, res, next) => {
+    console.log("token: " + req.cookies.token); 
+    
+    const url = process.env.PAYMENTS_URL + "/" + req.params.paymentid + "/captures";
+    console.log("paymentOS url: " + url)
+    const idempotencyKey = "cust-" + Math.floor(Math.random() * 1000) + "-payment";
+    
+    var myHeaders = new fetch.Headers();
+    myHeaders.append("app_id", process.env.APP_ID);
+    myHeaders.append("private_key", process.env.PRIVATE_KEY);
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("api-version", process.env.API_VERSION);
+    myHeaders.append("x-payments-os-env", process.env.X_PAYMENTS_OS_ENV);
+    myHeaders.append("idempotency_key", idempotencyKey);
+
+    var authorizationRequest = {
+        method: 'POST',
+        headers: myHeaders,
+        redirect: 'follow'
+    }
+
+    await fetch(url, authorizationRequest)
+    .then(response => {
+        if (response.ok) {
+            console.log("SUCCESS from paymentOS")
+            return response.json()
+        } else {
+            console.log("FAILURE from paymentOS")
+        }
+    })
+    .then(data => {
+        console.debug(data)  
+        //res.send(data)      
+        res.render('captures', { 
+            title: 'Refund Payment',
+            amount: data.amount,
+            currency: data.currency,
+            description: data.statement_soft_descriptor,
+            payment_id: req.params.paymentid
+        })
+    })
+    .catch(error => console.log("Error app.js: " + error.message))
+});
+
 module.exports = app;
---
+++ public/javascripts/captures.js
@@ -0,0 +1,47 @@
+
+console.log(document.cookie)
+
+function getCookieByName(cookieName) {
+    return document.cookie.split(';')
+    .map( ( x ) => { 
+    return x.trim()
+    .split( '=' ); 
+    })
+    .reduce(( a, b ) => {
+        a[ b[ 0 ] ] = b[ 1 ];
+        return a;
+    }, 
+    {})[cookieName];
+} 
+
+document.getElementById('refunds-form').addEventListener('submit', async(event) => {
+    event.preventDefault()
+    const url = "/payments/" + document.getElementById('payment_id').value + "/refunds";
+    console.log("sent Refunds request url: " + url);
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var authorizationRequest = {
+        method: 'POST',
+        headers: myHeaders,
+        redirect: 'follow'
+    }
+
+    await fetch(url, authorizationRequest)
+    .then(response => {
+        if (response.ok) {
+            console.log("SUCCESS")
+            return response.text()
+        } else {
+            console.log("FAILURE")
+        }
+    })
+    .then(data => {
+        document.open();
+        document.write(data)
+        document.close();
+    })
+    .catch(error => console.log("Error index.js: " + error.message))
+
+});
